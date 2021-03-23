89de54ffa41eab2bd520be97ee7108229ca11a81
Haider <haider-lab@gatech.edu>
2021-03-23 14:14:59 -0400

    Set use executable for camera interface to false as default

--- Core/primary_script.m
+++ Core/primary_script.m
@@ -8,14 +8,14 @@ addpath(genpath('General_Testing_Material'))
 global inputM cameraInterface;
 
 % Camera interface settings
-cameraInterface.useExecutable = true; %true if using executable to grab frames
+cameraInterface.useExecutable = false; %true if using executable to grab frames
 cameraInterface.exePath = 'C:\Users\haider-lab\Documents\MATLAB\ISI\Core\New_Imager\MdigProcess\vs2012\x64\Release\MDigProcess.exe'; %changed Debug to Release
 cameraInterface.tempStoragePath = 'C:/Users/haider-lab/Downloads/frames/'; %use local SSD for better performance
 cameraInterface.frameDimensions = [4090 3072]; %[width height] in pixels
 
 % Data output definitions
-inputM.mouseID = 'shea_test';
-inputM.date = '03_13';
+inputM.mouseID = 'Mouse_M210203_1';
+inputM.date = '03_22';
 inputM.analyzerRoot = 'Y:\haider\Data\ISI\Animal_Testing';
 inputM.ses = '1';
 
